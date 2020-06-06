226ff668d5cf0331c0b48909c29ae7362de4934c
Peter Levin <levipe01@gmail.com>
2020-06-06 10:23:33 -0600

    Set state with axios call

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,20 +6,29 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-
-    };
+      ticker: 'SPY',
+      cName: '',
+      desInfo: {}
+    }
 
   }
 
   componentDidMount() {
-
+    axios.get(`https://sandbox.iexapis.com/stable/stock/${this.state.ticker}/company?token=${config.app.api}`)
+    .then((response) => {
+      console.log(response)
+      this.setState({
+        cName: response.data.companyName,
+        desInfo: response.data
+      })
+    })
   }
 
   render() {
     return (
       <div>
         <h1>The Secaucus Observer</h1>
-        <Description />
+        <Description desInfo={this.state.desInfo}/>
       </div>
     );
   }
